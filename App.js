8f693f45605ccd018eecb5e1894931f11f0e3d70
Fael123Programming <rafaelfonseca1020@gmail.com>
2023-03-27 10:30:49 +0100

    added images

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
-      <LoginScreen/>
+      <WelcomeScreen/>
   );
 }
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { Text, ScrollView, View, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { 
+    Text, 
+    ScrollView, 
+    View, 
+    StyleSheet, 
+    TextInput, 
+    KeyboardAvoidingView, 
+    Platform,
+    Image
+} from 'react-native';
 import Header from '../components/Header';
 import colors from '../utils/Colors';
 
@@ -8,7 +17,7 @@ export default WelcomeScreen = () => {
 
     return (
         <KeyboardAvoidingView
-            style={welcomeScreenStyle.container}
+            style={welcomeStyle.container}
             behavior={Platform.OS === 'ios' ? 'padding' :  'height'}
         >   
             <Header text={'Greetings'}/>
@@ -17,21 +26,62 @@ export default WelcomeScreen = () => {
                 keyboardDismissMode={'on-drag'}
                 style={{ flex: .9}}
             >
-                <View style={{ flex: .1 }}>
-                    <Text style={welcomeScreenStyle.title} numberOfLines={2}>Welcome to Little{'\n'}Lemon</Text>
+                <View style={welcomeStyle.logoContainer}>
+                    <Image
+                        style={welcomeStyle.logo}
+                        source={require('../img/logo.png')}
+                        resizeMode={'cover'}
+                        accessible={true}
+                        accessibilityLabel={'Little Lemon logo'}
+                    />
+                    <Text style={welcomeStyle.logoText}>Little Lemon</Text>
                 </View>
-                <View style={{ flex: .75}}>
-                    <Text style={welcomeScreenStyle.welcomeText}>
+                <View>
+                    <Text 
+                        style={welcomeStyle.title} 
+                        numberOfLines={3}
+                    >Welcome to Little Lemon, your local mediterranean bistro</Text>
+                </View>
+                <View>
+                    <Text style={welcomeStyle.welcomeText}>
                         Little Lemon is a charming neighborhood bistro that serves 
                         simple food and classic cocktails in a lively but casual environment. 
                         We would love to hear more about your experience with us!
                     </Text>
                 </View>
                 <TextInput
-                    style={welcomeScreenStyle.input}
+                    style={welcomeStyle.input}
                     value={username}
                     onChangeText={onChangeUsername}
                     placeholder={'Full name'}
+                />
+                <Image
+                    style={welcomeStyle.image}
+                    source={require('../img/paper.png')}
+                    resizeMode={'cover'}
+                    accessible={true}
+                    accessibilityLabel={'Menu paper'}
+                />
+                <Image
+                    style={welcomeStyle.image}
+                    source={require('../img/lobster.png')}
+                    resizeMode={'cover'}
+                    accessible={true}
+                    accessibilityLabel={'Lobster'}
+                />
+                <Image
+                    style={welcomeStyle.image}
+                    source={require('../img/orange.png')}
+                    resizeMode={'cover'}
+                    accessible={true}
+                    accessibilityLabel={'Orange'}
+                />
+                <Image
+                    style={welcomeStyle.image}
+                    source={require('../img/pork_with_potatoes.png')}
+                    resizeMode={'cover'}
+                    accessible={true}
+                    accessibilityLabel={'Pork with potatoes'}
                 />
                 <Footer/>
             </ScrollView>
@@ -39,20 +89,34 @@ export default WelcomeScreen = () => {
     );
 }
 
-const welcomeScreenStyle = StyleSheet.create({
+const welcomeStyle = StyleSheet.create({
     container: { 
         flex: 1, 
-        backgroundColor: colors.black
+        backgroundColor: colors.black,
     },
     title: { 
-        fontSize: 30, 
+        fontSize: 26, 
         color: colors.white, 
         textAlign: 'center', 
-        padding: 30
+        padding: 30,
+        fontStyle: 'italic'
+    },
+    logoText: {
+        fontSize: 26, 
+        color: colors.white, 
+        textAlign: 'center', 
+        fontStyle: 'italic',
+        padding: 20
+    },
+    logoContainer: { 
+        flexDirection: 'row', 
+        alignItems: 'center',
+        alignSelf: 'center',
+        padding: 50
     },
     welcomeText: { 
-        color: 'white', 
-        fontSize: 28, 
+        color: colors.white, 
+        fontSize: 26, 
         textAlign: 'center', 
         padding: 30
     },
@@ -61,11 +125,23 @@ const welcomeScreenStyle = StyleSheet.create({
         margin: 12,
         borderWidth: 1,
         padding: 10,
-        marginBottom: 175,
+        marginBottom: 50,
         fontSize: 16,
         borderColor: colors.black,
         backgroundColor: colors.white,
         color: colors.black,
-        flex: .1
+    },
+    logo: {
+        height: 100,
+        width: 100,
+        alignSelf: 'center'
+    },
+    image: {
+        height: 300,
+        width: 300,
+        alignSelf: 'center',
+        margin: 30,
+        borderWidth: 1,
+        borderColor: colors.black
     }
 });
