9effee946c6b6686941b0645bb295b7138b39645
Fael123Programming <rafaelfonseca1020@gmail.com>
2023-03-28 09:27:10 +0100

    added icons to drawer

--- navigators/HomeMenuNavigator.js
+++ navigators/HomeMenuNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import MenuScreen from '../screens/MenuScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,7 +10,21 @@ const HomeMenuNavigator = () => {
     return (
         <Drawer.Navigator 
             useLegacyImplementation
-            screenOptions={{ headerShown: true, headerTitleAlign: 'center'}}
+            screenOptions={({ route }) => ({
+                headerTitleAlign: 'center',
+                drawerPosition: 'left',
+                drawerIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = focused ? 'home-sharp' : 'home-outline';
+                    } else if (route.name === 'Menu') {
+                        iconName = focused ? 'book' : 'book-outline';
+                    }
+                    return <Ionicons name={iconName} color={color} size={size}/>
+                },
+                drawerActiveTintColor: 'black',
+                drawerInactiveTintColor: 'grey'
+            })}
             initialRouteName={'Home'}
         >
             <Drawer.Screen name='Home' component={HomeScreen}/>
